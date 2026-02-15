3c009edc18fe552369b983cf21cb58cb6fc04ae0
Rhea Addepalli <rheaaddepalli315@gmail.com>
2026-02-15 18:36:54 +0530

    English to German deploy fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -61,15 +61,9 @@
 //   }
 // }
 
-
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
-
-interface LanguageOption {
-  code: string;
-  label: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -77,36 +71,24 @@ interface LanguageOption {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   userText = '';
   translatedText = '';
   loading = false;
 
-  sourceLang = 'en';   // Keep English as default and fixed
-targetLang = 'hi';   // Users can change this
+  // Fixed languages
+  sourceLang = 'en';
+  targetLang = 'de';
 
-languages: LanguageOption[] = [
-  // Only target languages are selectable, source is fixed to English
-  { code: 'hi', label: 'Hindi' },
-  { code: 'es', label: 'Spanish' },
-  { code: 'fr', label: 'French' },
-  { code: 'de', label: 'German' },
-];
-
-
-  // sourceLang = 'en';
-  // targetLang = 'hi';
-
-  // languages: LanguageOption[] = [
-  //   { code: 'en', label: 'English' },
-  //   { code: 'hi', label: 'Hindi' },
-  //   { code: 'es', label: 'Spanish' },
-  //   { code: 'fr', label: 'French' },
-  //   { code: 'de', label: 'German' },
-  // ];
+  // Only German allowed
+  languages = [
+    { code: 'de', label: 'German' }
+  ];
 
   constructor(private http: HttpClient) {}
 
   translate() {
+
     if (!this.userText.trim()) {
       alert('Please enter some text!');
       return;
@@ -115,21 +97,171 @@ languages: LanguageOption[] = [
     this.loading = true;
     this.translatedText = '';
 
+    // Backend now expects ONLY text
     const payload = {
-      text: this.userText,
-      src_lang: this.sourceLang,
-      tgt_lang: this.targetLang
+      text: this.userText
     };
 
-    this.http.post<any>(`${environment.apiUrl}/translate`, payload).subscribe({
-      next: (res) => {
-        this.translatedText = res.translated_text;
-        this.loading = false;
-      },
-      error: (err) => {
-        alert('Error calling API: ' + err.message);
-        this.loading = false;
-      }
-    });
+    this.http.post<any>(`${environment.apiUrl}/translate`, payload)
+      .subscribe({
+        next: (res) => {
+          this.translatedText = res.translated_text;
+          this.loading = false;
+        },
+        error: (err) => {
+          alert('Error calling API: ' + err.message);
+          this.loading = false;
+        }
+      });
+
   }
 }
+
+
+// for only english to all
+// import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { environment } from '../environments/environment';
+
+// interface LanguageOption {
+//   code: string;
+//   label: string;
+// }
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   userText = '';
+//   translatedText = '';
+//   loading = false;
+
+//   sourceLang = 'en';   // Keep English as default and fixed
+// targetLang = 'hi';   // Users can change this
+
+// languages: LanguageOption[] = [
+//   // Only target languages are selectable, source is fixed to English
+//   { code: 'hi', label: 'Hindi' },
+//   { code: 'es', label: 'Spanish' },
+//   { code: 'fr', label: 'French' },
+//   { code: 'de', label: 'German' },
+// ];
+
+
+//   // sourceLang = 'en';
+//   // targetLang = 'hi';
+
+//   // languages: LanguageOption[] = [
+//   //   { code: 'en', label: 'English' },
+//   //   { code: 'hi', label: 'Hindi' },
+//   //   { code: 'es', label: 'Spanish' },
+//   //   { code: 'fr', label: 'French' },
+//   //   { code: 'de', label: 'German' },
+//   // ];
+
+//   constructor(private http: HttpClient) {}
+
+//   translate() {
+//     if (!this.userText.trim()) {
+//       alert('Please enter some text!');
+//       return;
+//     }
+
+//     this.loading = true;
+//     this.translatedText = '';
+
+//     const payload = {
+//       text: this.userText,
+//       src_lang: this.sourceLang,
+//       tgt_lang: this.targetLang
+//     };
+
+//     this.http.post<any>(`${environment.apiUrl}/translate`, payload).subscribe({
+//       next: (res) => {
+//         this.translatedText = res.translated_text;
+//         this.loading = false;
+//       },
+//       error: (err) => {
+//         alert('Error calling API: ' + err.message);
+//         this.loading = false;
+//       }
+//     });
+//   }
+// }
+
+
+// for all languages 
+
+// import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { environment } from '../environments/environment';
+
+// interface LanguageOption {
+//   code: string;
+//   label: string;
+// }
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   userText = '';
+//   translatedText = '';
+//   loading = false;
+
+// //   sourceLang = 'en';   // Keep English as default and fixed
+// // targetLang = 'hi';   // Users can change this
+
+// // languages: LanguageOption[] = [
+// //   // Only target languages are selectable, source is fixed to English
+// //   { code: 'hi', label: 'Hindi' },
+// //   { code: 'es', label: 'Spanish' },
+// //   { code: 'fr', label: 'French' },
+// //   { code: 'de', label: 'German' },
+// // ];
+
+
+//   sourceLang = 'en';
+//   targetLang = 'hi';
+
+//   languages: LanguageOption[] = [
+//     { code: 'en', label: 'English' },
+//     { code: 'hi', label: 'Hindi' },
+//     { code: 'es', label: 'Spanish' },
+//     { code: 'fr', label: 'French' },
+//     { code: 'de', label: 'German' },
+//   ];
+
+//   constructor(private http: HttpClient) {}
+
+//   translate() {
+//     if (!this.userText.trim()) {
+//       alert('Please enter some text!');
+//       return;
+//     }
+
+//     this.loading = true;
+//     this.translatedText = '';
+
+//     const payload = {
+//       text: this.userText,
+//       src_lang: this.sourceLang,
+//       tgt_lang: this.targetLang
+//     };
+
+//     this.http.post<any>(`${environment.apiUrl}/translate`, payload).subscribe({
+//       next: (res) => {
+//         this.translatedText = res.translated_text;
+//         this.loading = false;
+//       },
+//       error: (err) => {
+//         alert('Error calling API: ' + err.message);
+//         this.loading = false;
+//       }
+//     });
+//   }
+// }
